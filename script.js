24b690337fd16d2bc0ab2b88531468b2c4c65e54
Alex <avmetzger@gmail.com>
2019-12-04 18:42:49 -0600

    finished lab, testing extras

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 // TODO: add code here
+window.addEventListener("load", function(){
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
+        response.json().then(function(json){
+            console.log(json);
+            let container = document.getElementById("container");
+            for (i=0; i< json.length; i++){
+                container.innerHTML += `
+                <div class="astronaut">
+                <div class="bio">
+                    <h3>${json[i].firstName} ${json[i].lastName}</h3>
+                    <ul>
+                        <li>Hours in space: ${json[i].hoursInSpace}</li>
+                        <li class="status">Active: ${json[i].active}</li>
+                        <li>Skills: ${json[i].skills.join(", ")}</li>
+                    </ul>
+                </div>
+                <img class="avatar" src="${json[i].picture}">
+                </div>`
+            }
+            let status = document.getElementsByClassName("status");
+            for(i=0;i<json.length; i++){
+                if (status[i].innerHTML === "Active: true"){
+                    status[i].style.color = "green";
+                }
+            }
+            console.log(status);
+        });
+    });
+});
